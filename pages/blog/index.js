c8faaf0d22ab4e5fa5531b61f3e7085af7bf7a86
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-01-12 20:03:48 -0300

    Polishing the Index Page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -2,7 +2,11 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../../components/layout";
 import utilStyles from "../../styles/utils.module.css";
 
-import { getSortedPostsData } from '../../lib/posts';
+
+import Link from 'next/link';
+import Date from '../../components/date';
+
+import { getSortedPostsData } from "../../lib/posts";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -13,8 +17,7 @@ export async function getStaticProps() {
   };
 }
 
-
-export default function Home({allPostsData}) {
+export default function Home({ allPostsData }) {
   return (
     <Layout>
       <Head>
@@ -24,11 +27,11 @@ export default function Home({allPostsData}) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
+
+
 import { getSortedPostsData } from '../lib/posts';
 
 export async function getStaticProps() {
