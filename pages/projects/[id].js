2a79ad94606606479b6a6e667969a626f7fa85c7
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-01-21 11:46:10 -0300

    Working with dynamic parameters

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,3 +1,22 @@
-export default function DetailProject() {
-    return <h2>Nome do projeto</h2>
+import path from "path";
+
+export default function DetailProject(props) {
+  const { loadedProject } = props;
+  return <h2>Nome do projeto: {loadedProject.title}</h2>;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const projectId = params.id;
+  const filePath = path.join(process.cwd(), "data", "projects.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  const project = data.projects.find((project) => project.id === projectId);
+
+  return {
+    props: {
+        loadedProject: project
+    }
+  }
 }
